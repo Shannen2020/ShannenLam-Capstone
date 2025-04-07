1a27e2c557692a103564532d97231d289c34c0c9
shannen2020 <shannen.lam@gmail.com>
2025-04-07 21:07:30 +0800

    organised file directory

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import StockDropdown from './stockdropdown'
+import StockDropdown from './components/stockdropdown'
 
 function App() {
   return (
